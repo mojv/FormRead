201384104a32e1dfd1ab854552e25ade5339819d
oscar <omunoz@dynamo-media.com>
2021-11-12 22:58:46 -0500

    refactor naming

---
+++ src/components/loadingModal.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fixed z-10 inset-0 overflow-y-auto bg-gray-800 bg-opacity-30 flex justify-center items-center" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+    <div
+        class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900 "
+    ></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
