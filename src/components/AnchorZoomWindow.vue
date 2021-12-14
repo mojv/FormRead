283f66f92a2610a8be7ee89e29572483d553dbfd
Oscar Muñoz <okar20@gmail.com>
2021-12-14 15:11:55 -0500

    refactor zoom area

--- src/components/AnchorZoomWindow.vue
+++ src/components/AnchorZoomWindow.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="absolute flex items-center justify-center  w-full h-full">
-    <div class="w-32 h-32 z-10 top-0.5">
-      <img class="rounded-full border border-gray-100 shadow-sm" :src="src" alt="user image" />
-    </div>
+    <img class="w-32 h-32 rounded-full border-gray-200 border-4 shadow-2xl" :src="src" alt="user image" />
   </div>
-
 </template>
 
 <script>
