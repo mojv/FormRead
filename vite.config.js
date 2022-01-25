7fbf32661b9fd78ae4ff0569af2a7f64e07fcd0a
Oscar Muñoz <okar20@gmail.com>
2022-01-24 19:12:42 -0500

    add obfuscator

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
+import obfuscator from 'rollup-plugin-obfuscator';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        plugins: [ // <-- use plugins inside output to not merge chunks on one file
+          obfuscator({
+            fileOptions: {
+              // options
+            }
+          })
+        ]
+      }
+    },
+  }
 })
