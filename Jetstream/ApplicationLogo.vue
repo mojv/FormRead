babec2f040326265862284163b7a35556241efd8
Oscar Muñoz <okar20@gmail.com>
2022-03-23 20:28:09 -0400

    make it same as server version

---
+++ Jetstream/ApplicationLogo.vue
@@ -0,0 +1,4 @@
+<template>
+    <img  class="w-48 lazy loaded" alt="Logo" data-ll-status="loaded"
+          src="/img/logo.png">
+</template>
---
+++ Jetstream/AuthenticationCardLogo.vue
@@ -0,0 +1,15 @@
+<template>
+    <img  class="w-48 lazy loaded" alt="Logo" data-ll-status="loaded"
+          src="/img/logo.png">
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import { Link } from '@inertiajs/inertia-vue3';
+
+    export default defineComponent({
+        components: {
+            Link,
+        },
+    })
+</script>
